4feaace8ef1126f2383283ac891e51375d13c613
lodiii <66790114+lodiii@users.noreply.github.com>
2020-06-19 15:22:46 +0200

    Update script.py

--- twitterBot/script.py
+++ twitterBot/script.py
@@ -31,8 +31,14 @@ randomR = random.choice(results)
 
 myList = [f'In {randomR[1]}, {randomR[2]} casualties due to HIV/AIDS were recorded in {randomR[0]}.',f'{randomR[0]} recorded an approximate of {randomR[2]} deaths related to HIV/AIDS in {randomR[1]}.',f'HIV/AIDS was responsible for more than {randomR[2]} deaths in {randomR[1]}, in {randomR[0]}.',f'At the end of {randomR[1]} it was reported that {randomR[2]} people died due to HIV/AIDS in {randomR[0]}.',f'More than {randomR[2]} people died in {randomR[1]} in {randomR[0]}. The cause was HIV or AIDS. This shows us the gravity of the situation.',f'If people had been more educated on HIV and AIDS, the death toll of {randomR[1]} in {randomR[0]} - {randomR[2]} - would have certainly been lower.',f'At the end of {randomR[1]}, more than {randomR[2]} deaths were reported in {randomR[0]}.',f'In {randomR[0]}, more than {randomR[2]} deaths by HIV/AIDS were recorded at the end of {randomR[1]}. This is largely due to a lack of education/protection.',f'The Government registered an approximate of {randomR[2]} deaths due to HIV/AIDS in {randomR[0]}, at the end of {randomR[1]}.']
 
-api.update_status(random.choice(myList))
+def job():
+    api.update_status(random.choice(myList))
     
+schedule.every(240).minutes.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
 
   
